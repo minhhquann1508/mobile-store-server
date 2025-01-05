e18a1deb6d88677c760740a435a1c3b45be3308c
Nguyen Minh Quan <minhhquann1508@gmail.com>
2025-01-05 13:39:17 +0700

    hash password user model

---
+++ src/controllers/authController.js
@@ -0,0 +1,5 @@
+import UserModel from '../models/User';
+
+const register = async (req, res) => {
+
+}
--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -39,6 +40,14 @@ const UserSchema = new mongoose.Schema({
         default: 'active'
     }
 }, { timestamps: true });
+
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified("password")) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
 
 const UserModel = mongoose.model('User', UserSchema);
 
